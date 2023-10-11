9c539b9137a3514d6c9be0d084bf85fe217166ae
Hayland Montalvo <hayland.montalvo@udla.edu.ec>
2023-10-11 08:49:41 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HaylandMontalvo_Cf1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
